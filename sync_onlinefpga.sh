80642bcd057df45f95d07e41a6a668330b704a6e
Kevin Jan <kevin.jan.tw@gmail.com>
2026-03-20 18:14:55 +0800

    Update sync_onlinefpga.sh

--- sync_onlinefpga.sh
+++ sync_onlinefpga.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-echo hls00-passwd | sudo -S chattr -i /home/boleduuser/onlinefpga.pyc
+[ -f /home/boleduuser/onlinefpga.pyc ] && echo hls00-passwd | sudo -S chattr -i /home/boleduuser/onlinefpga.pyc
 echo hls00-passwd | sudo -S cp -f __pycache__/onlinefpga.cpython-38.pyc /home/boleduuser/onlinefpga.pyc
 echo hls00-passwd | sudo -S chattr +i /home/boleduuser/onlinefpga.pyc
 
-echo hls00-passwd | sudo -S chattr -i /home/boleduuser/config.pyc
+[ -f /home/boleduuser/config.pyc ] && echo hls00-passwd | sudo -S chattr -i /home/boleduuser/config.pyc
 echo hls00-passwd | sudo -S cp -f __pycache__/config.cpython-38.pyc /home/boleduuser/config.pyc
 echo hls00-passwd | sudo -S chattr +i /home/boleduuser/config.pyc
 echo "synchronize config.pyc onlinefpga.pyc to OnlineFPGA boleduuser"
