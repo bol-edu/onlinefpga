f5819b25151c485c0887949291c3a892d34099cf
Kevin Jan <kevin.jan.tw@gmail.com>
2026-03-20 19:07:29 +0800

    Update sync_db.sh

--- sync_db.sh
+++ sync_db.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 echo hls00-passwd | sudo -S rm -rf "/opt/labManageKit/backupdb/$(date +"%Y-%m-%d")"
 echo hls00-passwd | sudo -S mkdir "/opt/labManageKit/backupdb/$(date +"%Y-%m-%d")"
 echo hls00-passwd | sudo -S cp -rf /mnt/LabData/hls00/boledudb "/opt/labManageKit/backupdb/$(date +"%Y-%m-%d")"
